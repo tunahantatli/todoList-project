e1c91b32edc2993e72c37d2af2c7938eba5c5d66
Tunahan Tatli <tunahantatl@gmail.com>
2022-09-06 21:58:56 +0300

    transfer-todo-elmn-frm-array

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const input = document.querySelector("#txtTaskName");
 const btnAddNewTask = document.querySelector("#btnAddNewTask");
 const btnDeleteAll = document.querySelector("#btnDeleteAll");
 const taskList = document.querySelector("#task-list");
+const items = ["Todo 1", "Todo 2","Todo 3","Todo 4"];
 
-eventListeners()
+//load items
+loadItems();
+
+eventListeners();
 function eventListeners(){
     //submit event
     form.addEventListener("submit",addNewItem);
@@ -16,6 +20,27 @@ function eventListeners(){
 //delete all item
 btnDeleteAll.addEventListener("click",deleteAllitems);
 
+function loadItems(){
+    items.forEach(function(item){
+        createItem(item);
+    })
+}
+
+function createItem(text){
+        // create li
+        const li = document.createElement("li");
+        li.className = "list-group-item list-group-item-secondary";
+        li.appendChild(document.createTextNode(text));
+        //create a
+        const a = document.createElement("a");
+        a.classList = "delete-item float-right";
+        a.setAttribute("href","#");
+        a.innerHTML = '<i class="fas fa-times"></i>'; 
+    
+        // add created elements to list
+        li.appendChild(a);
+        taskList.appendChild(li);
+}
 
 function addNewItem(e){
     if(input.value===''){
@@ -23,20 +48,7 @@ function addNewItem(e){
         console.log("submit");
 
     }
-    // create li
-    const li = document.createElement("li");
-    li.className = "list-group-item list-group-item-secondary";
-    li.appendChild(document.createTextNode(input.value));
-    //create a
-    const a = document.createElement("a");
-    a.classList = "delete-item float-right";
-    a.setAttribute("href","#");
-    a.innerHTML = '<i class="fas fa-times"></i>'; 
-
-    // add created elements to list
-    li.appendChild(a);
-    taskList.appendChild(li);
-
+    createItem(input.value);
 
     input.value ="";
 
@@ -45,8 +57,9 @@ function addNewItem(e){
 
 //delete ann elements
 function deleteItem(e){
-    if(confirm("Are you sure you want to delete?")){
+    
         if(e.target.className==="fas fa-times"){
+            if(confirm("Are you sure you want to delete?")){
             // console.log(e.target);
             e.target.parentElement.parentElement.remove()
          }
