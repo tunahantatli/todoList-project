83936db031fa45532980a30680c4e8e5d293fc3e
Tunahan Tatli <tunahantatl@gmail.com>
2022-09-07 01:15:24 +0300

    bugs-fixed

--- script.js
+++ script.js
@@ -11,97 +11,98 @@ let todos;
 loadItems();
 
 eventListeners();
-function eventListeners(){
-    //submit event
-    form.addEventListener("submit",addNewItem);
-    //delete ann item
-    taskList.addEventListener("click",deleteItem);
+function eventListeners() {
+  //submit event
+  form.addEventListener("submit", addNewItem);
+  //delete ann item
+  taskList.addEventListener("click", deleteItem);
 }
 //delete all item
-btnDeleteAll.addEventListener("click",deleteAllitems);
+btnDeleteAll.addEventListener("click", deleteAllitems);
 
-function loadItems(){
-    todos=getItemsFromLS();
-    todos.forEach(function(item){
-        createItem(item);
-    }) 
+function loadItems() {
+  todos = getItemsFromLS();
+  todos.forEach(function (item) {
+    createItem(item);
+  });
 }
 //get items from local storage
-function getItemsFromLS(){
-    if(localStorage.getItem("todos")=== null){
-        todos =  [];
-    }else{
-        todos = JSON.parse(localStorage.getItem("todos"));
-    }
-    return todos;
+function getItemsFromLS() {
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  return todos;
 }
 //set ıtem to local stroge
-function setItemToLS(newTodo){
-    todos = getItemsFromLS();
-    todos.push(newTodo);
-    localStorage.setItem("todos",JSON.stringify(todos));
+function setItemToLS(newTodo) {
+  todos = getItemsFromLS();
+  todos.push(newTodo);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function createItem(newTodo){
-        // create li
-        const li = document.createElement("li");
-        li.className = "list-group-item list-group-item-secondary";
-        li.appendChild(document.createTextNode(newTodo));
-        //create a
-        const a = document.createElement("a");
-        a.classList = "delete-item float-right";
-        a.setAttribute("href","#");
-        a.innerHTML = '<i class="fas fa-times"></i>'; 
-    
-        // add created elements to list
-        li.appendChild(a);
-        taskList.appendChild(li);
+function createItem(newTodo) {
+  // create li
+  const li = document.createElement("li");
+  li.className = "list-group-item list-group-item-secondary";
+  li.appendChild(document.createTextNode(newTodo));
+  //create a
+  const a = document.createElement("a");
+  a.classList = "delete-item float-right";
+  a.setAttribute("href", "#");
+  a.innerHTML = '<i class="fas fa-times"></i>';
+
+  // add created elements to list
+  li.appendChild(a);
+  taskList.appendChild(li);
 }
 
-function addNewItem(e){
-    if(input.value===''){
-        alert("opps! add new item")
-        console.log("submit");
+function addNewItem(e) {
+  if (input.value === "") {
+    alert("opps! add new item");
+    console.log("submit");
+    return;
+  }
 
-    }
+  setItemToLS(input.value);
+  createItem(input.value);
+  input.value = "";
 
-    setItemToLS(input.value);
-    createItem(input.value);
-    input.value ="";
-
-    e.preventDefault();
+  e.preventDefault();
 }
 
 //delete ann elements
-function deleteItem(e){
-    
-        if(e.target.className==="fas fa-times"){
-            if(confirm("Are you sure you want to delete?")){
-            // console.log(e.target);
-            e.target.parentElement.parentElement.remove();
-            deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
-        }
+function deleteItem(e) {
+  if (e.target.className === "fas fa-times") {
+    if (confirm("Are you sure you want to delete?")) {
+      // console.log(e.target);
+      e.target.parentElement.parentElement.remove();
+      deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
     }
-
+  }
 }
 //delete todo items from local storage
-function deleteTodoFromStorage(deletetodo){
-    let todos = getItemsFromLS();
-    todos.forEach(function(todo,index){
-        if(todo===deletetodo){
-            todos.splice(index,1);
-        }
-    });
-    localStorage.setItem(todos,JSON.stringify(todos));
+function deleteTodoFromStorage(deletetodo) {
+  let todos = getItemsFromLS();
+  todos.forEach(function (todo, index) {
+    if (todo === deletetodo) {
+      todos.splice(index, 1);
+    }
+  });
+  localStorage.setItem(todos, JSON.stringify(todos));
 }
 //delete all items
-function deleteAllitems(e){
-    if(confirm("Are you sure you want to delete all items?")){
-        while(taskList.firstChild){
-            taskList.removeChild(taskList.firstChild);
-        }
-        localStorage.clear();
-        //taskList.innerHTML="";
+function deleteAllitems(e) {
+  if (taskList.length >= 1) {
+    if (confirm("Are you sure you want to delete all items?")) {
+      while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+      }
+      localStorage.clear();
+      //taskList.innerHTML="";
     }
-    
+  }else{
+    alert("the list is clear!");
+  }
 }
